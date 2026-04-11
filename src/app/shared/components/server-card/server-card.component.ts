757515d6223138fd96d3c6e90376d9a76fc3693d
Elia Giolli <eliagiolli22@gmail.com>
2026-04-11 12:41:50 +0200

    repo initialized and first steps done

---
+++ src/app/shared/components/server-card/server-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-card',
+  imports: [],
+  templateUrl: './server-card.component.html',
+  styleUrl: './server-card.component.css'
+})
+export class ServerCardComponent {
+
+}
